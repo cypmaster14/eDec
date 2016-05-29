be07fb7195d8b4f18d0d7a4c3df407799ab7fb4b
GripcaGeorge <george.gripca@gmail.com>
2016-05-29 16:47:00 +0300

    Added new search filters + infinite scroll

--- www/js/searchProductCtrl.js
+++ www/js/searchProductCtrl.js
@@ -1,7 +1,4 @@
-﻿angular.module('edec').controller("searchProduct",['$scope','$http','$window', '$ionicPopup','$anchorScroll', '$timeout','$state','pagina','produs',function ($scope,$http,$window,$ionicPopup,$anchorScroll,$timeout,$state,pagina,produs) {
-
-
-  $scope.products=[];
+﻿angular.module('edec').controller("searchProduct",['$scope','$http','$window', '$ionicPopup','$anchorScroll', '$timeout','$state','$rootScope','pagina','produs',function ($scope,$http,$window,$ionicPopup,$anchorScroll,$timeout,$state,$rootScope,pagina,produs) {
 
   $scope.showAlert=function(titlu,mesaj)
   {
@@ -10,9 +7,9 @@
         template:mesaj
       });
 
-        alertPopup.then(function (res) {
+        /*alertPopup.then(function (res) {
          console.log("You clicked Ok");
-      });
+      });*/
   };
 
   $scope.clickOnProduct=function (produs) {
@@ -20,118 +17,78 @@
      $state.go("tabs.product",{'ok':'ok','barcode':produs});
   };
 
-
-  $scope.previous=function () {
-     pagina=pagina-2;
-     $scope.search('yes');
-     $anchorScroll('top-nav');
-  };
-
-
-  $scope.search=function (option) {
-
-      $scope.cautat=true;
-      $scope.aux=0;
-      if(!$scope.searchedProduct)
+  $scope.getNumberOfRecords=function(){
+	  $scope.existaNext=false;
+	  $scope.products=[];
+	  pagina=0;
+	  $scope.fakePage=0;
+	  if(!$scope.searchedProduct)
       {
         $scope.showAlert('Search',"Enter a product");
         return;
       }
-
-      if(option.localeCompare('no')==0)
-      {
-        produs=$scope.searchedProduct;
-        pagina=1;
-      }
-      else
-        pagina=pagina+1;
-
-
-      // if(produs.localeCompare($scope.searchedProduct)==0)
-      // {
-
-      //   pagina=pagina+1;
-      // }
-      // else
-      // {
-      //   produs=$scope.searchedProduct;
-      //   pagina=1;
-      // }
-      $scope.showAlert('Search','You search'+$scope.searchedProduct+" pagina"+pagina);
-
-
-      if(1==1)
-      {
-        var numarProduse=$http.get('https://nodeserve-cypmaster14.c9users.io/numarProduse?product='+$scope.searchedProduct);
+	  if ($rootScope.user==null){
+		$rootScope.user='';
+	  } 
+	  if ($rootScope.user!=''){
+		  $scope.displayPreferences=true;
+	  }else{
+		  $scope.displayPreferences=false;
+	  }
+	  var numarProduse=$http.get('https://nodeserve-cypmaster14.c9users.io/numarProduse?product='+$scope.searchedProduct);
 
         numarProduse.success( function (data,status,headers,config) {
 
           if(status==200)
            {
-              $scope.nrProduse=data.linii;
-               if(data.linii!=0)
-               {
-                 console.log("Numar produse in functie:"+$scope.nrProduse);
-                 console.log("Pagina:"+pagina);
-                  if(pagina*10-10<=0) // am pus 10 pt ca am 10 produse pe pagina daca vreau sa modific trebuie sa modific si din serve
-                      $scope.existaPrevious=false;
-                  else
-                      $scope.existaPrevious=true;
-
-                  if(pagina*10<data.linii)
-                     $scope.existaNext=true;
-                   else
-                       $scope.existaNext=false;
-               }
-
-               console.log("Next:"+$scope.existaNext);
-               console.log("Previous:"+$scope.existaPrevious);
+              $scope.numarProduse=data.linii;
                $anchorScroll('top-nav');
-
+			  if ($scope.numarProduse>0) {
+				  $scope.search();
+			  }
+			   else
+              {
+                $scope.showAlert("Find","Product was not found");
+              }
           }
        });
 
        numarProduse.error(function  (data,status,headers,config) {
                $scope.showAlert("Error on request",status+' '+headers);
             });
+  }
+  
+  $scope.search=function (option) {
+	$scope.fakePage++;
+	if ($scope.fakePage%2==1){
+		pagina++;
+		$scope.showAlert('Search',"Pagina "+pagina);
+		console.log($rootScope.user);
+		var res=$http.get('https://nodeserve-cypmaster14.c9users.io/products?product='+$scope.searchedProduct+"&pagina="+pagina+"&user="+$rootScope.user);
 
+		res.success(function (data,status,headers,config) {
 
-      }
+			if(status==200)
+			{
+				$scope.text=data.text;
+				if($scope.text.localeCompare("Gasit")==0) //produsul a fost gasit
+				{
+					$scope.products.push.apply($scope.products,data.products);
+				}
+			}
 
+		});
 
-
-      var res=$http.get('https://nodeserve-cypmaster14.c9users.io/products?product='+$scope.searchedProduct+"&pagina="+pagina);
-
-      res.success(function (data,status,headers,config) {
-
-          if(status==200)
-          {
-              $scope.text=data.text;
-              if($scope.text.localeCompare("Gasit")==0) //produsul a fost gasit
-              {
-
-                  $scope.products=data.products;
-                  $scope.paginaExista=true;
-                  console.log($scope.paginaExista);
-                  console.log("Numar produse in afara:"+$scope.nrProduse);
-
-
-
-                  console.log("next:" +$scope.existaNext);
-                  console.log("previous:" +$scope.existaPrevious);
-              }
-              else
-              {
-                $scope.showAlert("Find","Product was not found");
-              }
-          }
-
-      });
-
-      res.error(function  (data,status,headers,config) {
+		res.error(function  (data,status,headers,config) {
             $scope.showAlert("Error on request",status+' '+headers);
-        });
-
+		});
+		if(pagina*10<$scope.numarProduse) {
+			$scope.existaNext=true;
+		} else {
+			$scope.existaNext=false;
+		}
+	}
+	$scope.$broadcast('scroll.infiniteScrollComplete');
   };
 
 }]);
