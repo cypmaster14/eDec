ff482e845903f3263bca38961a59673011bdd57e
Claudiu1995 <alexandruclaudiu95@gmail.com>
2016-05-30 00:48:48 +0300

    editare pagina campanie

--- www/js/campaignCtrl.js
+++ www/js/campaignCtrl.js
@@ -1,7 +1,8 @@
 angular.module('edec').controller('CampaignCtrl', ['$scope', '$state', '$stateParams', '$http', '$ionicPopup', '$timeout', '$rootScope', '$ionicActionSheet', 'logat', 'user', function ($scope, $state, $stateParams, $http, $ionicPopup, $timeout, $rootScope, $ionicActionSheet, logat, user) {
+	
 	if ($stateParams.campaign_id != "empty") {
         $scope.campaign_id = $stateParams.campaign_id;
-		alert("da");
+		alert($scope.campaign_id);
 	}
 	else{
 		alert("empty");
